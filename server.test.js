2fcbb4b1ddc42599e2ef8af2c5da32b5d1e196ec
Joni Anttila <jonianttila89@hotmail.com>
2022-10-25 09:32:28 +0300

    Added test

---
+++ server.test.js
@@ -0,0 +1,13 @@
+const app = require("../server.js");
+
+describe("app.test", function () {
+  it("GET / should return the homepage", function (done) {
+    request(app)
+      .get("/")
+      .end(function (err, res) {
+        if (err) return done(err);
+        expect(res.status).to.eq(200);
+        done();
+      });
+  });
+});
