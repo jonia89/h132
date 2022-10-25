3a097a15d49354bf32dbea4dc9ce663d14f240fb
Joni Anttila <jonianttila89@hotmail.com>
2022-10-25 10:06:12 +0300

    Splitted app and server and test for expressjs

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+app.get("/", (req, res) => {
+  console.log("Request to path /");
+  res.send(`You throwed a dice and got ${Math.floor(Math.random() * 6) + 1}`);
+});
+module.exports = app.js;
